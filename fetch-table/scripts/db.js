46235b03674e7db39814979887a6979b9acf5b76
Potyeszka <dielmolbor@gmail.com>
2025-04-17 18:24:13 +0200

    java

---
+++ fetch-table/scripts/db.js
@@ -0,0 +1,12 @@
+const apiUrl = 'https://nettuts.hu/jms/Potyeszka/';
+
+export const getAll = (entity = 'products') =>{
+    return fetch(`${apiUrl}${entity}`)
+    .then(response => response.json());
+};
+export const remove = (id=0,entity = 'products') =>{
+    return fetch(`${apiUrl}${entity}/${id}`,{
+        method: 'DELETE',
+    })
+    .then(response => response.json());
+};
